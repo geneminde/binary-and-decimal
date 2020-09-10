7eaba3d09d158c07eb339580bf38397625a901f7
geneminde <30448498+geneminde@users.noreply.github.com>
2020-09-10 12:33:21 -0700

    Update binary_to_decimal.rb

--- lib/binary_to_decimal.rb
+++ lib/binary_to_decimal.rb
@@ -5,5 +5,21 @@
 # Calculate  and return the decimal value for this binary number using
 # the algorithm you devised in class.
 def binary_to_decimal(binary_array)
-  raise NotImplementedError
+  if binary_array.length == 1
+    return binary_array[0]
+  else
+    return binary_array[0] * 2**(binary_array.length - 1) + binary_to_decimal(binary_array.slice(1...binary_array.length))
+  end
+end
+
+# The method 'decimal_to_binary' takes any decimal integer and converts 
+# it to an array filled with 1s and 0s representing the binary integer 
+# equal to decimal 
+
+def decimal_to_binary(decimal)
+  if decimal == 0 || decimal == 1
+    return [decimal]
+  else
+    return decimal_to_binary(decimal/2) + [decimal % 2]
+  end
 end
